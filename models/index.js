4be6842116130b8c41783251ab2520c9b3e8a0f1
Margaux <margaux.santos113@gmail.com>
2024-02-13 23:59:39 -0800

    index.js, and db

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Blogpost = require('./Blogpost');
+
+
+User.hasMany(Blogpost, { 
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blogpost.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Blogpost.hasMany(Comment, {
+    foreignKey: 'blogpost_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Comment, Blogpost };
