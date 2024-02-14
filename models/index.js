ca09f7ce9dba20f4aea937ec4badff10c2ad96b8
Margaux <margaux.santos113@gmail.com>
2024-02-14 03:05:00 -0800

    main.handlebars and models index.js

--- models/index.js
+++ models/index.js
@@ -2,30 +2,30 @@ const User = require('./User');
 const Comment = require('./Comment');
 const Blogpost = require('./Blogpost');
 
-
+// users can have many posts
 User.hasMany(Blogpost, { 
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
-
+// blogposts belong to a single user
 Blogpost.belongsTo(User, {
     foreignKey: 'user_id'
 });
-
+// blogposts can have many comments 
 Blogpost.hasMany(Comment, {
     foreignKey: 'blogpost_id',
     onDelete: 'CASCADE'
 });
-
+// comments belong to a single user
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
-
+// user can have many comments
 User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
   });
-
+// a single comment belongs to a single user
 Comment.belongsTo(Post, {
     foreignKey: 'post_id'
 });
